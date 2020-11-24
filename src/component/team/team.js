c1090668f5c12de8d29363befb55794de17060f7
ChengCalvin <calvincheng_95@hotmail.com>
2020-11-24 01:00:04 -0500

    check data

--- src/component/team/team.js
+++ src/component/team/team.js
@@ -35,7 +35,7 @@ class Team extends Component {
     this.setState({ isloading: true });
     axios.get("https://guessthelolteam.herokuapp.com/gamedata").then((res) => {
       this.setState({ data: res.data, isloading: false });
-      console.log(data);
+      console.log(this.state.data);
     });
     this.setState({ isButtonDisabled: false });
   };
