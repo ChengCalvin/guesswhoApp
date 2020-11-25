37ec080a36c4acc5be8c7854fc0163d7822d320a
ChengCalvin <calvincheng_95@hotmail.com>
2020-11-25 10:03:16 -0500

    add routing to signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Team from "./component/team/team";
 import SignUpButton from "./component/Signup/SignUpButton";
+import SignUpPage from "./component/Signup/SignUpPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
@@ -13,7 +15,10 @@ const App = () => {
             <h1>League of Legend Team Guessing Game</h1>
           </div>
           <div>
-            <SignUpButton />
+            <Router>
+              <Route path="/signup" component={SignUpPage} />
+              <SignUpButton />
+            </Router>
           </div>
         </div>
       </header>
