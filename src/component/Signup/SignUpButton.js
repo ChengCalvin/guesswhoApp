95b116f969db465942b8e005b6a8a20af7a0b747
ChengCalvin <calvincheng_95@hotmail.com>
2020-11-25 10:45:19 -0500

    remove history

--- src/component/Signup/SignUpButton.js
+++ src/component/Signup/SignUpButton.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./Signup.css";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SignUpButton = () => {
-  const historyTracker = () => {
-    useHistory().push("/signuppage");
-  };
   return (
     <div>
       <Link to="/signuppage">
