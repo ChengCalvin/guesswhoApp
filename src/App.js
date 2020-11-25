d075756aaf4f8e276ad5da005f5a1c764077e62a
ChengCalvin <calvincheng_95@hotmail.com>
2020-11-25 09:39:08 -0500

    add login button and header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 import Team from "./component/team/team";
+import SignUpButton from "./component/Signup/SignUpButton";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <header className="header">
-        <title>League of Legend Team Guessing Game</title>
+        <div className="headertitle">
+          <h1>League of Legend Team Guessing Game</h1>
+        </div>
+        <div>
+          <SignUpButton />
+        </div>
       </header>
       <h1>Guess which league of legend team won</h1>
       <Team />
---
+++ src/component/Signup/SignUpButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Signup.css";
+
+const SignUpButton = () => (
+  <div>
+    <button className="signupbtn">Signup</button>
+  </div>
+);
+
+export default SignUpButton;
