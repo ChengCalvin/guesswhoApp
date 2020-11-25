188b48fac22962dee95de8081b81e77df3f37bcf
ChengCalvin <calvincheng_95@hotmail.com>
2020-11-25 09:45:06 -0500

    header update

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ const App = () => {
   return (
     <div className="App">
       <header className="header">
-        <div className="headertitle">
-          <h1>League of Legend Team Guessing Game</h1>
-        </div>
-        <div>
-          <SignUpButton />
+        <div className="flexaligntitleandbtn">
+          <div className="headertitle">
+            <h1>League of Legend Team Guessing Game</h1>
+          </div>
+          <div>
+            <SignUpButton />
+          </div>
         </div>
       </header>
       <h1>Guess which league of legend team won</h1>
