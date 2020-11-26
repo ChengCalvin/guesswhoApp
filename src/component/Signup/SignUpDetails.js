776b0dab2fe65a314e56dbd15876aeb4358e68d8
ChengCalvin <calvincheng_95@hotmail.com>
2020-11-25 19:05:10 -0500

    add form to signup page

--- src/component/Signup/SignUpDetails.js
+++ src/component/Signup/SignUpDetails.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 // form, submit, call api endpoint and send data to database
@@ -17,7 +18,8 @@ const SignUpDetails = () => {
     console.log("value", event.target.value);
   };
 
-  const submitFormHandler = (event) => {
+  const submitFormHandler = () => {
+    console.log("newUser", newUser);
     const user = {
       firstName: newUser.firstName,
       lastName: newUser.lastName,
@@ -25,58 +27,61 @@ const SignUpDetails = () => {
       password: newUser.password,
       password_confirm: newUser.password_confirm,
     };
+
+    axios.post("/api/users", user).then(request);
     console.log(user);
-    console.log("newUser", newUser);
   };
 
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-          value={newUser.firstName}
-          onChange={inputChangeHandler}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          value={newUser.lastName}
-          onChange={inputChangeHandler}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          value={newUser.email}
-          onChange={inputChangeHandler}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="password"
-          name="password"
-          value={newUser.password}
-          onChange={inputChangeHandler}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Confirm Password"
-          name="password_confirm"
-          value={newUser.password_confirm}
-          onChange={inputChangeHandler}
-        />
-      </div>
-      <button onClick={() => submitFormHandler}>Create User</button>
+      <form onClick={submitFormHandler}>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            value={newUser.firstName}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value={newUser.lastName}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={newUser.email}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="password"
+            name="password"
+            value={newUser.password}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Confirm Password"
+            name="password_confirm"
+            value={newUser.password_confirm}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <button type="submit">Create User</button>
+      </form>
     </div>
   );
 };
